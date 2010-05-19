4a607126ef5c7dfbaf330a29f43075b446871762
Daniel Maturana <dimatura@puc.cl>
2010-05-19 15:30:01 -0400

    Added copyright notices (GPLv3)

--- src/hdf5opencv.hh
+++ src/hdf5opencv.hh
@@ -1,3 +1,20 @@
+//  Copyright (C) 2010 Daniel Maturana
+//  This file is part of hdf5opencv.
+// 
+//  hdf5opencv is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+// 
+//  hdf5opencv is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+// 
+//  You should have received a copy of the GNU General Public License
+//  along with hdf5opencv. If not, see <http://www.gnu.org/licenses/>.
+// 
+
 #ifndef HDF5OPENCV_KVHWT01S
 #define HDF5OPENCV_KVHWT01S
 
